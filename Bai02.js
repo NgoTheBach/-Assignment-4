ebeb9db29b60916fb658998caa0f5afebab33eb6
Ngô Thế Bách <96104099+NgoTheBach@users.noreply.github.com>
2022-04-22 14:12:16 +0700

    second commit

--- Bai02.js
+++ Bai02.js
@@ -2,7 +2,7 @@ const array = [1,2,3,1,2,1,1];
 // const array = [1, 2, 3, 1, 2, 7, 7, 1, 1, 4, 5];
 const counter = {};
 
-function Delete(array) {
+function checkForElementAppearMedium(array) {
     array.forEach(name => counter[name] ? counter[name]++ : counter[name] = 1);
 
     const min = Math.min(...Object.values(counter));
@@ -11,4 +11,4 @@ function Delete(array) {
     const medium = Object.entries(counter).filter(([name, reps]) => reps !== min && reps !== max);
     return medium
 }
-console.log(Delete(array));
+console.log(checkForElementAppearMedium(array));
